6789c6da4a7be3602afb856ac021fc9283f4365b
medhatmebed <medhat.mebed@gmail.com>
2023-12-21 06:36:02 +0200

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Medhat Mebed on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
